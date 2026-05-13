18c090780044dda6d841bf14ff7f922392740341
Matteo Seminara <matteo@semaconsulting.com>
2026-05-13 21:54:08 +0200

    Initial Commit

---
+++ CrossFFB/CrossFFBApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrossFFBApp.swift
+//  CrossFFB
+//
+//  Created by teo on 13/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct CrossFFBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
